9f6867632c13f51cb76993a7d32b3aa2ea4db56a
Dennis Kimaita <dkimaita22@gmail.com>
2023-09-03 04:32:59 +0300

    create customers table in sqlalchemy

---
+++ app.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, Sequence, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# define the database connection
+DATABASE_URI = 'sqlite:///restaurants.db'# path to database
+engine = create_engine(DATABASE_URI, echo=True)
+
+# Base class for all the classes
+Base = declarative_base()
+
+class Customers(Base):
+    __tablename__='customer'
+    customer_id = Column(Integer, Sequence('customer_id_seq'), primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    
+
+# creating all the tablesk
+Base.metadata.create_all(bind=engine)
+
+# create session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+session.close()
+
